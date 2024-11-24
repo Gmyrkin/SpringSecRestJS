734d3e65e4a0f805c9f0e412b963efab56e09901
Gmyrkin <gmyrkin@gmail.com>
2024-11-24 18:41:33 +0300

    List Users in console

--- src/main/java/ru/spb/db/rest/spring/exmp315/SpringRestAPI/handler/UserNotCreatedException.java
+++
@@ -1,8 +0,0 @@
-package ru.spb.db.rest.spring.exmp315.SpringRestAPI.handler;
-
-public class UserNotCreatedException extends RuntimeException {
-
-    public UserNotCreatedException (String msg){
-        super(msg); // передаю в RuntimeException сообщение об ошибке из UserErrorResponse
-    }
-}
